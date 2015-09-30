6ac1f7b679ef951f162158b114cd907fd0df9314
Henry <lefevrehenry@hotmail.fr>
2015-09-30 16:49:41 +0200

    swag

--- Outils info/29-09-15/test.R
+++ Outils info/29-09-15/test.R
@@ -1,58 +1,83 @@
 rm(list=ls())
 
-rotation <- function(pic, rot=90) {
+rotation_opti90 <- function(pic) {
   matrix <- as.matrix(pic)
-  dim <- dim(matrix)
-  
-  
-  if (rot == 90) {
-    res <- matrix(0, dim[2], dim[1])
-    for (i in 1:dim[2]) {
-      res[i,] <- rev(matrix[,i])
-    }
-  }
-  if (rot == 180) {
-    res <- rotation(matrix, 90)
-    res <- rotation(res, 90)
-  }
-  if (rot == 270) {
-    res <- rotation(matrix, 90)
-    res <- rotation(res, 90)
-    res <- rotation(res, 90)
-  }
-  
-  return ( res )
+  return ( apply(t(matrix), 2, rev) )
+}
+
+rotation_opti180 <- function(pic) {
+  matrix <- rotation_opti90(pic)
+  matrix <- rotation_opti90(matrix)
+  return ( matrix )
+}
+
+rotation_opti270 <- function(pic) {
+  matrix <- rotation_opti90(pic)
+  matrix <- rotation_opti90(matrix)
+  matrix <- rotation_opti90(matrix)
+  return ( matrix )
 }
 
 quantize <- function(pic, k=1) {
   image(as.matrix(pic), col = gray.colors(2^k))
 }
 
-reduction <- function(pic, m) {
-  dim <- dim(pic)
+reduction_opti <- function(pic, m) {
   matrix <- as.matrix(pic)
-  
-  tmp <- matrix(0, dim[1]/m, dim[2])
-  for (i in 1:dim[1]) {
-    tmp[i/m,] <- matrix[i,]
-  }
-  
-  res <- matrix(0,dim[1]/m, dim[2]/m)
-  for (i in 1:dim[2]) {
-    res[,i/m] <- tmp[,i]
-  }
-  
-  return ( res )
+  dim <- dim(matrix)
+  seq_line <- seq(1, dim[1], m)
+  seq_column <- seq(1, dim[2], m)
+  return ( matrix[seq_line, seq_column] )
 }
 
+
+compression <- function(pic, k=1) {
+  matrix <- as.matrix(pic)
+  svd <- svd(matrix, k)
+  d <- svd$d
+  u <- svd$u
+  v <- svd$v
+  return ( u[,1:k] %*% diag(d[1:k]) %*% t(v[,1:k]) )
+}
+
+distance <- function(matrix1, matrix2) {
+  dim1 <- dim(matrix1)
+  dim2 <- dim(matrix2)
+  if (dim1[1] == dim2[1] && dim1[2] == dim2[2]) {
+    return ( sqrt( sum( (matrix1-matrix2)^2 )) )
+  } else {
+    return ( -1 ) # GG Noob !
+  }
+}
+
+
+################################################
 pic <- read.csv(file="Documents/M2/M2-S1/Outils info/29-09-15/lena.csv", head=TRUE, sep=",")
 image(as.matrix(pic), col = gray.colors(256))
 
-#res <- rotation(img, rot=270)
-#image(res)
+res <- rotation_opti90(pic)
+image(res)
+
+res <- rotation_opti180(pic)
+image(res)
+
+res <- rotation_opti270(pic)
+image(res)
 
 #res <- quantize(pic, 3)
 
-#res <- reduction(pic, 5)
+#res <- reduction_opti(pic, 2)
 #image(res)
 
+m <- as.matrix(pic)
+res1 <- compression(pic, 2)
+res2 <- compression(pic, 10)
+res3 <- compression(pic, 15)
+res4 <- compression(pic, 30)
+
+distance(m, res1)
+distance(m, res2)
+distance(m, res3)
+distance(m, res4)
+
+image(res4)
